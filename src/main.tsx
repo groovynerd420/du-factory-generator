ac74413441715b1f51f954e44ef155bc75636788
Pedro Matos <pedrohugorm@gmail.com>
2022-10-22 01:51:32 -0500

    Dark Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
-import "antd/dist/antd.css"
+import "antd/dist/antd.dark.css"
 import { App } from "./ui/app"
 
 const rootElement = document.getElementById("root")
